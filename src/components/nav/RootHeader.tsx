d39e28c8a11d8be3daa080e21bcbc79769ce544d
ROG\zyc199513 <709837842@qq.com>
2023-02-27 19:30:58 +0800

    hide Header ui

--- src/components/nav/RootHeader.tsx
+++ src/components/nav/RootHeader.tsx
@@ -6,20 +6,20 @@ export const RootHeader = () => {
       <div className="mx-4 flex max-w-[1120px] flex-auto items-center justify-between">
         <img className="w-[140px]" src="/img/topia.svg" alt="" />
         <div className="flex justify-between">
-          <div className="flex cursor-pointer items-center justify-between pl-12">
-            <p className="mr-3">Build</p>
-            <ArrowDropDownIcon className="w-5" />
-          </div>
-          <div className="flex cursor-pointer items-center justify-between pl-12">
-            <p className="mr-3">Learn</p>
-            <ArrowDropDownIcon className="w-5" />
-          </div>
+          {/*<div className="flex cursor-pointer items-center justify-between pl-12">*/}
+          {/*  <p className="mr-3">Build</p>*/}
+          {/*  <ArrowDropDownIcon className="w-5" />*/}
+          {/*</div>*/}
+          {/*<div className="flex cursor-pointer items-center justify-between pl-12">*/}
+          {/*  <p className="mr-3">Learn</p>*/}
+          {/*  <ArrowDropDownIcon className="w-5" />*/}
+          {/*</div>*/}
           <div className="flex cursor-pointer items-center justify-between pl-12">
             <p className="mr-3">Community</p>
             <ArrowDropDownIcon className="w-5" />
           </div>
           <div className="pl-12">Whitepaper</div>
-          <div className="pl-14">About Us</div>
+          {/*<div className="pl-14">About Us</div>*/}
         </div>
       </div>
     </div>
