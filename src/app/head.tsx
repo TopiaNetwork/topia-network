502df3c340e65bc3a00fb8503e1987a73f3f5afa
ROG\zyc199513 <709837842@qq.com>
2023-02-27 21:22:11 +0800

    Add head information

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Topia</title>
+      <title>Topia | Home</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Topia" />
+      <meta name="description" content="EVM Compatible Storage Layer2" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
