94de38e4e450e526a07cf1397eed83187171597a
ROG\zyc199513 <709837842@qq.com>
2023-02-28 20:58:58 +0800

    change title

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Topia | Home</title>
+      <title>Topia - EVM Compatible Storage Layer2</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="EVM Compatible Storage Layer2" />
       <link rel="icon" href="/favicon.ico" />
